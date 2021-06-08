1b438a6230042714a22087fd08f573914eaa9056
guilherme chaves <guilgchaves@protonmail.com>
2021-06-08 19:52:53 -0300

    feat(hidden-menu): hidden menu added for mobile;

---
+++ assets/script/script.js
@@ -0,0 +1,24 @@
+console.log('quack quack')
+
+const menu = document.getElementById('menu')
+const exit = document.getElementById('menu-exit')
+const hiddenMenu = document.getElementById('hidden')
+
+menu.addEventListener('click', (event) => {
+  console.log('clicked')
+  event.preventDefault()
+  openNav()
+})
+
+exit.addEventListener('click', (event) => {
+  event.preventDefault()
+  closeNav()
+})
+
+function openNav() {
+  hiddenMenu.style.height = '100%'
+}
+
+function closeNav() {
+  hiddenMenu.style.height = '0%'
+}
